39e51762ce284c4ce9a2090d5621c3bb79c098e6
M4XK8R <kpopmyhope@gmail.com>
2024-03-17 14:37:06 +0300

    Initial commit

---
+++ app/src/main/java/com/maxkor/cryptocoins/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.maxkor.cryptocoins
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.maxkor.cryptocoins.ui.theme.CryptocoinsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocoinsTheme {
+
+            }
+        }
+    }
+}
